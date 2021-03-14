18a0426724047d3398ad59e58ee22253ba9a24bf
etileyli <etileyli@hotmail.com>
2021-03-14 15:00:20 +0300

    open&close a file

--- Codes/example_02.04.c
+++ Codes/example_02.04.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
+
 
 int main(int argc, char const *argv[]) {
 
-  int fildes;
+  // int fildes = open("/etc/passwdd", O_RDONLY);
+  int fildes = open("/etc/passwd", O_RDONLY); // O_RDONLY in fcntl.h (https://pubs.opengroup.org/onlinepubs/007904875/functions/open.html)
 
-  if (close(fildes) == -1)
-    perror("Failed to close the file.");
+
+  if (fildes == -1){
+    perror("Failed to open the file.");
+  }
+  else{
+    printf("File is opened.\n");
+    if (close(fildes) == -1)
+      perror("Failed to close the file.");
+    else
+      printf("File is closed.\n");
+  }
 
   return 0;
 }
