7886827d4430429f54a68edf390a1827560ed994
etileyli <etileyli@hotmail.com>
2021-04-04 08:38:31 +0300

    First null version of function parse.

--- shell/TURKSHELL.c
+++ shell/TURKSHELL.c
@@ -4,6 +4,24 @@
 
 #define EXIT_KEYWORD "exit"
 
+// Type s_command stores user command's parsed information.
+typedef struct s_command{
+  int isBackground;
+  char **argv;
+  int argc;
+}s_command;
+
+// This function parses user command into s_command variables.
+s_command *parse(char *myArgv){
+
+  s_command *cmd = (s_command*)malloc(sizeof(s_command));
+
+  cmd->isBackground = 0;
+  cmd->argc = 2;
+
+  return cmd;
+}
+
 char *readLine(){
   char *buffer = NULL;  // input line
   size_t bufsize = 512; // max size of input line
@@ -30,13 +48,22 @@ int main(int argc, char const *argv[]) {
 
   printf("TURKSHELL'e Hosgeldiniz!\n");
 
+  s_command *cmd;
+
+  // shell's loop
   do{
     char *line = readLine();
     printf("Your input is: %s\n", line);
 
+    // parse input command
+    cmd = parse(line);
+
+    printf("isBackground = %d; argv = %d\n", cmd->isBackground, cmd->argc);
+
+    // check exit condition
     if (!strcmp(line, EXIT_KEYWORD)){
-      printf("GOODBYE MESSAGE!\n");
-      free(line);
+      printf("CIKIYOR!\n");
+      free(line); // free allocated memory in readLine function
       break;
     }
 
