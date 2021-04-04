f77550131987851c5444a0ed1ed27394b9062e44
etileyli <etileyli@hotmail.com>
2021-04-04 10:47:08 +0300

    parser c and h files are added.

--- shell/TURKSHELL.c
+++ shell/TURKSHELL.c
@@ -1,73 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "parser.h"
 
 #define EXIT_KEYWORD "exit"
-
-// Type s_command stores user command's parsed information.
-typedef struct s_command{
-  int isBackground;
-  int argc;
-  char **argv;
-}s_command;
-
-// This function parses user command into s_command variables.
-s_command *parse(char *myArgv){
-
-  s_command *cmd = (s_command*)malloc(sizeof(s_command));
-
-  const char s[2] = " ";
-  char *token;
-
-  cmd->argv = calloc(1, sizeof(char *));
-
-  /* get the first token */
-  int i = 0;
-  token = strtok(myArgv, s);
-
-  /* walk through other tokens */
-  while( token != NULL ) {
-     // printf( " %s\n", token );
-     cmd->argv[i++] = token;
-     cmd->argv = realloc(cmd->argv, sizeof(char *)*(i + 1));
-     token = strtok(NULL, s);
-  }
-
-  // an extra NULL is required to terminate the array
-  cmd->argv[i] = NULL;
-
-  cmd->argc = i;
-
-  // check if the process is background
-  if (!strcmp(cmd->argv[i-1], "&"))
-    cmd->isBackground = 1;
-  else
-    cmd->isBackground = 0;
-
-  return cmd;
-}
-
-char *readLine(){
-  char *buffer = NULL;  // input line
-  size_t bufsize = 512; // max size of input line
-  size_t charCount;     // character number of input line
-
-  // allocate memory for input line
-  buffer = (char *)malloc(bufsize * sizeof(char));
-  if( buffer == NULL)
-  {
-    perror("Unable to allocate buffer");
-    exit(1);
-  }
-
-  // get number of characters
-  charCount = getline(&buffer, &bufsize, stdin);
-
-  // Truncate last newline character
-  buffer[charCount - 1] = '\0';
-
-  return buffer;
-}
 
 int main(int argc, char const *argv[]) {
 
---
+++ shell/parser.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Type s_command stores user command's parsed information.
+typedef struct s_command{
+  int isBackground;
+  int argc;
+  char **argv;
+}s_command;
+
+s_command *parse(char *myArgv);
+char *readLine();
