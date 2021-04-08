2ebfc02da720dbcbd7820027655e31f85f486171
etileyli <etileyli@hotmail.com>
2021-04-08 10:45:09 +0300

    Architecture change: 1 fork() only.

--- shell/TURKSHELL.c
+++ shell/TURKSHELL.c
@@ -49,6 +49,7 @@ int main(int argc, char const *argv[]) {
       break;
     }
 
+		// Test code
     // printf("isBackground = %d; argv = %d\n", cmd->isBackground, cmd->argc);
     // int i = 0;
     // while (cmd->argv[i]){
@@ -60,78 +61,62 @@ int main(int argc, char const *argv[]) {
     // for (int i = 0; i < numOfElements; i++){
     //   printf("%s\n", builtInCommands[i]);
     // }
-		/****************************Test command**********************************/
-    // Test command: yankı
-    if(!strcmp(cmd->argv[0], builtInCommands[0])){
-      // printf("The command is %s\n", cmd->argv[0]);
-      pid_t childPid;
 
-      // Fork
-      if ((childPid = fork()) < 0){
-        perror("error in fork()");
-        return -1;
-      }
-      else if (childPid == 0) {	// in child process
-        // printf("I'm child %ld, ID = %ld\n", (long)getpid(), (long)getppid());
+		pid_t childPid;
 
-        // Echo the words entered after 'yankı'
-        int j = 1;
-        while (cmd->argv[j]){
-          printf("%s ",cmd->argv[j]);
-          j++;
-        }
-        printf("\n");
+		// Fork
+		if ((childPid = fork()) < 0){
+			perror("error in fork()");
+			return -1;
+		}
+		else if (childPid == 0) {	// in child process
+			// printf("I'm child %ld, ID = %ld\n", (long)getpid(), (long)getppid());
 
-        return 0;
-      }
-      // parent process
-      else{
-        wait(&childPid);
-        // printf("I am parent %ld, ID = %ld\n", (long)getpid(), (long)getppid());
-      }
-    }
-		/****************************First command*********************************/
-		// birleştir (imitates 'cat' command)
-    else if(!strcmp(cmd->argv[0], builtInCommands[1])){
-      // printf("The command is %s\n", cmd->argv[0]);
-      pid_t childPid;
+			/****************************Test command********************************/
+			// Test command: yankı
+			if(!strcmp(cmd->argv[0], builtInCommands[0])){
+				int j = 1;
+				while (cmd->argv[j]){
+					printf("%s ",cmd->argv[j]);
+					j++;
+				}
+				printf("\n");
 
-      // Fork
-      if ((childPid = fork()) < 0){
-        perror("error in fork()");
-        return -1;
-      }
-      else if (childPid == 0) {	// in child process
-
-        /* *** First function of birleştir: Read file contents into stdout*** */
-        int fd;
-        char *filePath = cmd->argv[1];
-        if ((fd = open(filePath, O_RDONLY)) == -1)
-        {
-            perror("Cannot open file");
-            exit(1);
-        }
+				return 0;
+			}
+			/****************************First command*******************************/
+			// birleştir (imitates 'cat' command)
+			else if(!strcmp(cmd->argv[0], builtInCommands[1])){
+				/**1st Function of birleştir: Read file contents into stdout**/
+				int fd;
+				char *filePath = cmd->argv[1];
+				if ((fd = open(filePath, O_RDONLY)) == -1)
+				{
+						perror("Cannot open file");
+						exit(1);
+				}
 
 				// print file content to terminal
 				printFile(fd, filePath);
 				close(fd);
-				/* *** Second function of birleştir: Read all non-command arguments as
+				/**2nd Function of birleştir: Read all non-command arguments as
 				input files and print their content to terminal.*** */
 
-        return 0;	// Child returns
-      }
-      // parent process
-      else{
-        wait(&childPid);
-      }
-    }
-    else if(!strcmp(cmd->argv[0], builtInCommands[2])){
-      printf("The command is %s\n", cmd->argv[0]);
-    }
-    else{
-      printf("Command is not in the list!\n");
-    }
-
+				return 0;
+			}
+			else if(!strcmp(cmd->argv[0], builtInCommands[2])){
+				printf("The command is %s\n", cmd->argv[0]);
+				return 0;
+			}
+			else{
+				printf("Command is not in the list!\n");
+				return 0;
+			}
+		}
+		else{	// in parent process
+			wait(&childPid);
+			// printf("I am parent %ld, ID = %ld\n", (long)getpid(), (long)getppid());
+		}
   }while(1);
 
   return 0;
