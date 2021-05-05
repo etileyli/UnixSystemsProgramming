59908092726ba9bd737dbd7c522dfc6beb05ec7b
etileyli <etileyli@hotmail.com>
2021-05-05 14:27:14 +0300

    temp

--- scheduler/scheduler.c
+++ scheduler/scheduler.c
@@ -1,71 +1,42 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "ds.h"
+  #include <stdio.h>
+  #include <stdlib.h>
+  #include <unistd.h>
+  #include "ds.h"
 
+  void *scheduler(void *anyParam);
+  void TaskA(void *);
+  void TaskB(void *);
+  void TaskC(void *);
 
-int main(int argc, char const *argv[]) {
+  int main(int argc, char const *argv[]) {
 
-  queue *queFCFS = createQueue();
+    queue *queFCFS = createQueue();
 
-  // queue *quePB = createQueue();
+    // queue *quePB = createQueue();
 
-  int thread1, thread2, thread3, thread4, thread5, thread6, thread7;
+    int thread1, thread2, thread3, thread4, thread5, thread6, thread7;
 
-  // displayNode2(thread1);
+    void (*fun_ptr_arr[3])(void *) = {TaskA, TaskB, TaskC};
 
-  // enqueue_proc2(makeProc2(&thread1, 1), queFCFS);
-  // enqueue_proc2(makeProc2(&thread2, 2), queFCFS);
-  // enqueue_proc2(makeProc2(&thread3, 3), queFCFS);
-  // enqueue_proc2(makeProc2(&thread4, 4), queFCFS);
+    void *par = NULL;
+    int i = 0;
+    while(1){
+      (*fun_ptr_arr[i%3])(par);
+      sleep(1);
+      i++;
+    }
 
-  insert_proc2(makeProc2(&thread1, 1), queFCFS);
-  insert_proc2(makeProc2(&thread2, 2), queFCFS);
-  insert_proc2(makeProc2(&thread3, 3), queFCFS);
-  insert_proc2(makeProc2(&thread4, 4), queFCFS);
+    return 0;
+  }
 
-  // printf("The Queue:\n");
-  // displayQueue(queFCFS);
-  //
-  // dequeue_proc(queFCFS);
-  // dequeue_proc(queFCFS);
-  //
-  // printf("The Queue after deletion:\n");
-  // displayQueue(queFCFS);
+void TaskA(void *param){
+  printf("TaskA\n");
+}
 
-  // printf("The PCB Table:\n");
-  // displayPCBTable();
+void TaskB(void *param){
+  printf("TaskB\n");
+}
 
-  insert_proc2(makeProc2(&thread5, 0), queFCFS);
-  insert_proc2(makeProc2(&thread6, 8), queFCFS);
-  insert_proc2(makeProc2(&thread7, 3), queFCFS);
-
-  printf("The Queue:\n");
-  displayQueue(queFCFS);
-
-  // delete_proc2(thread5, queFCFS);
-  // delete_proc2(thread6, queFCFS);
-  // delete_proc2(thread7, queFCFS);
-
-  del_proc2(thread5, queFCFS);
-  del_proc2(thread6, queFCFS);
-  del_proc2(thread7, queFCFS);
-
-  printf("The Queue after deletion:\n");
-  displayQueue(queFCFS);
-
-  printf("The PCB Table:\n");
-  displayPCBTable();
-
-  insert_proc2(makeProc2(&thread5, 1), queFCFS);
-  insert_proc2(makeProc2(&thread6, 7), queFCFS);
-  insert_proc2(makeProc2(&thread7, 2), queFCFS);
-
-  printf("The Queue after re-insertation:\n");
-  displayQueue(queFCFS);
-
-  printf("The PCB Table:\n");
-  displayPCBTable();
-
-
-  return 0;
+void TaskC(void *param){
+  printf("TaskC\n");
 }
